c99d8d9c53a94471e5c5e656a525a736973811fc
dthaug <daniel_haugland@hotmail.com>
2013-08-21 22:33:17 +0200

    heroku commit 2

--- app/models/scramble.rb
+++ app/models/scramble.rb
@@ -10,10 +10,6 @@ class Scramble < ActiveRecord::Base
   validates :user_id, :description, :price, :image, presence: true
   validates :price, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, presence: true, :numericality => {:greater_than => 0}
  
-  validates_attachment :image,
-                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-                            size: { less_than: 5.megabytes }
-
   has_attached_file :image, styles: { medium: "400x400>",
                                       small:  "200x200#"}
 
