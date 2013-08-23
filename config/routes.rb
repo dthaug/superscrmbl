eb032c511bfb673f06aedfd282f74d82c8582f93
dthaug <daniel_haugland@hotmail.com>
2013-08-23 17:27:45 +0200

    Heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@
   resources :orders
   end
 
-devise_for :user, :controllers => { :registration => "registration", :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :user, :controllers => { :registration => "registration", :omniauth_callbacks => "omniauth_callbacks" }
 
-devise_scope :user do
-  get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
-end
+    devise_scope :user do
+      get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+    end
 
   root :to => "static_pages#home"
 
